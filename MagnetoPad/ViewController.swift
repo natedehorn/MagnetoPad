1f5fd508e597af44d26b7e5b179e211bb847823d
natedehorn <natedehorn@gmail.com>
2016-04-18 15:38:30 -0400

    Adding streaming drawing
    Version features:
    - Drawing by magnetometer data
    - Gathers magnetometer data at much quicker time interval
    - Custom line color
    
    Issues:
    - Magnetometer does not draw at intended time

--- MagnetoPad/ViewController.swift
+++ MagnetoPad/ViewController.swift
@@ -15,13 +15,17 @@ class ViewController: UIViewController {
     var currentPoint:CGPoint!
     
     var isSwiping:Bool!
-    var drawing:Bool!
+    var drawing = false
     var red:CGFloat!
     var green:CGFloat!
     var blue:CGFloat!
 
     var xmag:Double!
     var ymag:Double!
+    var xstart:Double!
+    var ystart:Double!
+    var xnext:Int!
+    var ynext:Int!
     
     let manager = CMMotionManager()
     
@@ -41,17 +45,9 @@ class ViewController: UIViewController {
         let xxx = Int(self.xval.text!)!
         let yyy = Int(self.yval.text!)!
         currentPoint = CGPoint(x: xxx, y: yyy)
+        drawBetweenPoints(lastPoint, end:currentPoint)
         print(currentPoint)
-        UIGraphicsBeginImageContext(self.imageView.frame.size)
-        self.imageView.image?.drawInRect(CGRectMake(0, 0, self.imageView.frame.size.width, self.imageView.frame.size.height))
-        CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y)
-        CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), currentPoint.x, currentPoint.y)
-        CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 2.0)
-        CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(),red, green, blue, 1.0)
-        CGContextStrokePath(UIGraphicsGetCurrentContext())
-        CGContextSetLineCap(UIGraphicsGetCurrentContext(), CGLineCap.Round)
-        self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
+    
         lastPoint = currentPoint
         xval.endEditing(true)
         yval.endEditing(true)
@@ -61,7 +57,27 @@ class ViewController: UIViewController {
         let x = Int(self.xval.text!)!
         let y = Int(self.yval.text!)!
         lastPoint = CGPoint(x: x, y: y)
-        print(lastPoint)
+    }
+    
+    @IBAction func startDrawing(sender: AnyObject) {
+        drawing = true
+    }
+    
+    @IBAction func stopDrawing(sender: AnyObject) {
+        drawing = false
+    }
+    
+    func drawBetweenPoints(start:CGPoint, end:CGPoint){
+        UIGraphicsBeginImageContext(self.imageView.frame.size)
+        self.imageView.image?.drawInRect(CGRectMake(0, 0, self.imageView.frame.size.width, self.imageView.frame.size.height))
+        CGContextMoveToPoint(UIGraphicsGetCurrentContext(), start.x, start.y)
+        CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), end.x, end.y)
+        CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 2.0)
+        CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(),red, green, blue, 1.0)
+        CGContextStrokePath(UIGraphicsGetCurrentContext())
+        CGContextSetLineCap(UIGraphicsGetCurrentContext(), CGLineCap.Round)
+        self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
     }
     
     override func viewDidLoad() {
@@ -75,19 +91,31 @@ class ViewController: UIViewController {
         xval.keyboardType = UIKeyboardType.NumberPad
         yval.keyboardType = UIKeyboardType.NumberPad
         
-        motionKit.getMagnetometerValues(0.5){
+        motionKit.getMagnetometerValues(){
             (x, y, z) in
             self.xmag = x
             self.ymag = y
-            print(self.xmag)
-            print(self.ymag)
+            print(self.xmag,",", self.ymag)
+            print(self.drawing)
             
             //Warn user that magnet is too close!
-            if y < 0 {
+            if self.ymag < 0 {
                 let alertController = UIAlertController(title: "MagnetoPad", message:"Magnet is too close to device!", preferredStyle: UIAlertControllerStyle.Alert)
                 self.presentViewController(alertController, animated: true, completion: nil)
                 alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
             }
+            
+            //Initialize the start point
+            if self.lastPoint == nil {
+                let xtemp = self.xmag
+                let ytemp = self.ymag
+                self.lastPoint = CGPoint(x: xtemp, y: ytemp)
+            }
+            
+            let point1 = CGPoint(x: self.xmag, y: self.ymag)
+            self.drawBetweenPoints(self.lastPoint, end: point1)
+            self.lastPoint = point1
+            
         }
     }
 }
